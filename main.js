525e8fb0fba24a721715e406721dfdc3f77027de
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-11 14:42:03 -0500

    created 'setFirstName'. test passed

--- main.js
+++ main.js
@@ -1,60 +1,61 @@
 // Your code here.
 
-function getFirstName(obj){
-    return obj.firstName
+function getFirstName(obj) {
+	return obj.firstName;
 }
 
-function getLastName(obj){
-  return obj.lastName
+function getLastName(obj) {
+	return obj.lastName;
 }
 
-function getFullName(obj){
-  return `${obj.firstName} ${obj.lastName}`
+function getFullName(obj) {
+	return `${obj.firstName} ${obj.lastName}`;
 }
 
-
+function setFirstName(obj, name) {
+  obj.firstName = name
+}
 
 // Our code here. Don't touch!
-if (typeof getFirstName === 'undefined') {
-  getFirstName = undefined
+if (typeof getFirstName === "undefined") {
+	getFirstName = undefined;
 }
 
-if (typeof getLastName === 'undefined') {
-  getLastName = undefined
+if (typeof getLastName === "undefined") {
+	getLastName = undefined;
 }
 
-if (typeof getFullName === 'undefined') {
-  getFullName = undefined
+if (typeof getFullName === "undefined") {
+	getFullName = undefined;
 }
 
-if (typeof setFirstName === 'undefined') {
-  setFirstName = undefined
+if (typeof setFirstName === "undefined") {
+	setFirstName = undefined;
 }
 
-if (typeof setAge === 'undefined') {
-  setAge = undefined
+if (typeof setAge === "undefined") {
+	setAge = undefined;
 }
 
-if (typeof giveBirthday === 'undefined') {
-  giveBirthday = undefined
+if (typeof giveBirthday === "undefined") {
+	giveBirthday = undefined;
 }
 
-if (typeof marry === 'undefined') {
-  marry = undefined
+if (typeof marry === "undefined") {
+	marry = undefined;
 }
 
-if (typeof divorce === 'undefined') {
-  divorce = undefined
+if (typeof divorce === "undefined") {
+	divorce = undefined;
 }
-
 
 module.exports = {
-  getFirstName,
-  getLastName,
-  getFullName,
-  setFirstName,
-  setAge,
-  giveBirthday,
-  marry,
-  divorce,
-}
+	getFirstName,
+	getLastName,
+	getFullName,
+	setFirstName,
+	setAge,
+	giveBirthday,
+	marry,
+	divorce,
+};
